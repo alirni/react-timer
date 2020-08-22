706813df0ca55574448b403e6905c3860a2dc59e
alirni <aliramezani19.1@gmail.com>
2020-08-22 23:47:40 +0430

    test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,15 @@
+// https://enzymejs.github.io/enzyme/docs/api
+// https://jestjs.io/docs/en/getting-started
+
 import React from 'react';
-import Enzyme from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-it('renders learn react link', () => {});
+test('renders learn react link', () => {
+  const wrapper = shallow(<App />);
+  console.log(wrapper.debug());
+  expect(wrapper).toBeTruthy();
+});
