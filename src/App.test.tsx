392eb083286fc8121fa2f71c64932a9127c4454d
alirni <aliramezani19.1@gmail.com>
2020-08-26 13:21:48 +0430

    fix(App.test): use functions

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -9,20 +9,41 @@ import TimerContainer from './component/TimerContainer';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * Create a ShallowWrapper for th App component.
+ * @function setup
+ * @param {object} props - Component props.
+ * @param {any} state - Initial state for setup.
+ * @returns {ShalloeWrapper}
+ */
+const setup = (props = {}, state = null) => {
+  return shallow(<App {...props} />);
+};
+
+/**
+ * Return ShalloeWrapper containing node(s) with the given data-test value.
+ * @param {ShalloeWrapper} wrapper - Enzyme shallow wrapper to search within.
+ * @param {string} value - Value of data-test attribute for search.
+ * @returns {ShalloeWrapper}
+ */
+const findByTestAttr = (wrapper, value) => {
+  return wrapper.find(`[data-test="${value}"]`);
+};
+
 test('renders without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find('.app');
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'appComponent');
   expect(appComponent.length).toBe(1);
 });
 
 test('renders TimerContainer', () => {
-  const wrapper = shallow(<App />);
-  const timerContainerComponent = wrapper.find('[data-test="timerContainerComponent"]');
+  const wrapper = setup();
+  const timerContainerComponent = findByTestAttr(wrapper, 'timerContainerComponent');
   expect(timerContainerComponent.length).toBe(1);
 });
 
 test('renders Timer display', () => {
   const wrapper = shallow(<TimerContainer />);
-  const timerComponent = wrapper.find('[data-test="timerComponent"]');
+  const timerComponent = findByTestAttr(wrapper, 'timerComponent');
   expect(timerComponent.length).toBe(1);
 });
